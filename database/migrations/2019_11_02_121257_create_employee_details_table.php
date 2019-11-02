a9cce0d497e8b5a36fe49137a3f2c61ebe5c8d1d
solomoreal <solomoreal@yahoo.com>
2019-11-02 15:45:11 +0100

    ...

--- database/migrations/2019_11_02_121257_create_employee_details_table.php
+++ database/migrations/2019_11_02_121257_create_employee_details_table.php
@@ -15,6 +15,12 @@ class CreateEmployeeDetailsTable extends Migration
     {
         Schema::create('employee_details', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('middle_name')->nullable();
+            $table->string('department')->nullable();
+            $table->string('position')->nullable();            
             $table->timestamps();
         });
     }
