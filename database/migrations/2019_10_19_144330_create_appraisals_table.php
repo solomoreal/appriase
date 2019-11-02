ef3f443d231e017eb2665a40cfb27adb92ccebc3
solomoreal <solomoreal@yahoo.com>
2019-11-02 13:40:11 +0100

    created appraisal table fields

--- database/migrations/2019_10_19_144330_create_appraisals_table.php
+++ database/migrations/2019_10_19_144330_create_appraisals_table.php
@@ -15,6 +15,22 @@ class CreateAppraisalsTable extends Migration
     {
         Schema::create('appraisals', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('reviewer_id')->nullable();
+            $table->unsignedBigInteger('reviewee_id')->nullable();
+            $table->enum('job_knowledge',['h','m','l'])->nullable()->comment('h=high,m=medium,l=low');
+            $table->enum('work_quality',['h','m','l'])->nullable()->comment('h=high,m=medium,l=low');
+            $table->enum('work_consistencies',['h','m','l'])->nullable()->comment('h=high,m=medium,l=low');
+            $table->enum('enthusiasm',['h','m','l'])->nullable()->comment('h=high,m=medium,l=low');
+            $table->enum('cooperation',['h','m','l'])->nullable()->comment('h=high,m=medium,l=low');
+            $table->enum('attitude',['h','m','l'])->nullable()->comment('h=high,m=medium,l=low');
+            $table->enum('initiative',['h','m','l'])->nullable()->comment('h=high,m=medium,l=low');
+            $table->enum('work_relation',['h','m','l'])->nullable()->comment('h=high,m=medium,l=low');
+            $table->enum('creativity',['h','m','l'])->nullable()->comment('h=high,m=medium,l=low');
+            $table->enum('attendance',['h','m','l'])->nullable()->comment('h=high,m=medium,l=low');
+            $table->enum('productivity',['h','m','l'])->nullable()->comment('h=high,m=medium,l=low');
+            $table->enum('dependability',['h','m','l'])->nullable()->comment('h=high,m=medium,l=low');
+            $table->enum('communication_skill',['h','m','l'])->nullable()->comment('h=high,m=medium,l=low');
+            $table->text('reviewer_comment')->nullable();
             $table->timestamps();
         });
     }
